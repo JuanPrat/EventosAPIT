d9dd52aee81d6b329bc42f463c7ecb9ebf54e76b
Juan Jose Garcia Sosa <juanprat96@gmail.com>
2022-01-03 22:28:08 -0500

    ajustes tabla crear empresa

--- src/pages/CreateCompanyPage.js
+++ src/pages/CreateCompanyPage.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useTable } from "react-table";
 import '../styles/AdministrationMainContent.css'
+
 
 
 export const CreateCompanyPage = () => {
@@ -7,6 +9,8 @@ export const CreateCompanyPage = () => {
     const handleAddPerson = () => {
 
     }
+
+    
 
     const data = [
         {
@@ -47,8 +51,16 @@ export const CreateCompanyPage = () => {
             Header: 'Estado',
             accessor: 'estado'
         },
-
     ]
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data })
+
 
     return (
         <div className="administration-main-content">
@@ -78,7 +90,50 @@ export const CreateCompanyPage = () => {
             </form>
             <button onClick={handleAddPerson}>Agregar</button>
             
-    
+            <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+       <thead>
+         {headerGroups.map(headerGroup => (
+           <tr {...headerGroup.getHeaderGroupProps()}>
+             {headerGroup.headers.map(column => (
+               <th
+                 {...column.getHeaderProps()}
+                 style={{
+                   borderBottom: 'solid 3px red',
+                   background: 'aliceblue',
+                   color: 'black',
+                   fontWeight: 'bold',
+                 }}
+               >
+                 {column.render('Header')}
+               </th>
+             ))}
+           </tr>
+         ))}
+       </thead>
+       <tbody {...getTableBodyProps()}>
+         {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                     style={{
+                       padding: '10px',
+                       border: 'solid 1px gray',
+                       background: 'papayawhip',
+                     }}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+     </table>
             
         </div>
     )
